e464d755931b7e93ad538a3d8d4165fe375809a0
KLZ-0 <adrian@kalazi.com>
2020-12-13 20:18:26 +0100

    Add "visible calendars"

--- app/src/main/java/com/kalazi/countdown/settings/SettingsFragment.java
+++ app/src/main/java/com/kalazi/countdown/settings/SettingsFragment.java
@@ -3,9 +3,15 @@ package com.kalazi.countdown.settings;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
+import androidx.preference.MultiSelectListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import com.kalazi.countdown.R;
+import com.kalazi.countdown.calendar.CalendarItem;
+import com.kalazi.countdown.calendar.CalendarManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     private SharedPreferences.OnSharedPreferenceChangeListener listener;
@@ -22,10 +28,33 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         // set up preferences from xml
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
+        fillCalendar();
+
         registerUIListeners();
     }
 
     ////// Private utility methods
+
+    private void fillCalendar() {
+        MultiSelectListPreference enabled_calendar_pref = findPreference("enabled_calendars");
+        if (enabled_calendar_pref == null) {
+            return;
+        }
+
+        // FIXME: Exception
+        List<CalendarItem> calendarItemList = CalendarManager.loadCalendars(requireContext());
+
+        List<String> entries = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (CalendarItem calendar : calendarItemList) {
+            entries.add(calendar.displayName);
+            values.add(Integer.toString(calendar.id));
+        }
+
+        enabled_calendar_pref.setEntries(entries.toArray(new CharSequence[0]));
+        enabled_calendar_pref.setEntryValues(values.toArray(new CharSequence[0]));
+    }
 
     /**
      * Register all UI listeners (UI Interactivity)
